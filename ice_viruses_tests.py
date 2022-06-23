d504b713ab0a288e519b62ab9a43318976e54056
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-06-23 17:24:20 +1000

    :zap: adding plot of root to tip regression

--- ice_viruses_tests.py
+++ ice_viruses_tests.py
@@ -9,5 +9,6 @@ def test_alignment(alignment: Alignment):
     alignment.assert_width(462)
 
 
-def test_root_to_tip(tree: Tree):
+def test_root_to_tip(tree: Tree, alignment: Alignment):
     tree.assert_root_to_tip(min_r_squared=0.5, min_rate=0.001, max_rate=0.009, min_root_date=1800, max_root_date=1900)
+    tree.plot_root_to_tip("root-to-tip.pdf", covariation=True, sequence_length=alignment.get_alignment_length())
