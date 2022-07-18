f63b1ce91c94b17920831cdf7af5e73651c45273
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-07-18 18:34:24 +1000

    :zap: adding plot for other tests too

--- ice_viruses_tests.py
+++ ice_viruses_tests.py
@@ -1,14 +1,15 @@
 from phytest import Tree
+from typing import List
 
 
-def test_root_to_tip_r_squared(tree: Tree):
-    tree.assert_root_to_tip(min_r_squared=0.5)
+def test_root_to_tip_r_squared(tree: Tree, extra:List):
+    tree.assert_root_to_tip(min_r_squared=0.5, extra=extra)
 
 
-def test_root_to_tip_rate(tree: Tree):
-    tree.assert_root_to_tip(min_rate=0.002, max_rate=0.005)
+def test_root_to_tip_rate(tree: Tree, extra:List):
+    tree.assert_root_to_tip(min_rate=0.002, max_rate=0.005, extra=extra)
 
 
-def test_root_to_tip_date(tree: Tree, extra):
+def test_root_to_tip_date(tree: Tree, extra:List):
     tree.assert_root_to_tip(min_root_date=1800, max_root_date=1900, extra=extra)
 
